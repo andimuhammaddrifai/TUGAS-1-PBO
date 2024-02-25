2eccdda4afa0095447e489ffcf10da739303b386
andimuhammaddrifai <161203129+andimuhammaddrifai@users.noreply.github.com>
2024-02-25 17:38:27 +0800

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,12 @@
+public class P1 {
+/**
+* @param args
+*/
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+float f= 20.0f;
+double fll;
+/* Algoritma */
+fll=10.0f;
+System.out.println ("f : "+f+ "\nf11: "+fll);
+} }
